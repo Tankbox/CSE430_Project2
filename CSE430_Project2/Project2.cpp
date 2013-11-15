54ba6aa2a784863dcc177f6dec0b830afe8c1f9a
Joe Cowen-Richards <joe@richcows.com>
2013-11-14 17:32:26 -0700

    Added test case 1 and working on reading files

--- CSE430_Project2/Project2.cpp
+++ CSE430_Project2/Project2.cpp
@@ -1,7 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool readGraph(const char* graph)
+{
+	ifstream myFile(*graph, "r");
+	
+	int vertices;
+	cin >> vertices;
+	printf("%d", vertices);
+	return false;
+}
 
 int main( int argc, const char* argv[] )
 {
+	readGraph(argv[0]);
 	printf( "\nHello World\n\n" );
 }
