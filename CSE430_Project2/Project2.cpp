90196b918428bc5ace90e20d6c2349327d574131
Joe Cowen-Richards <joe@richcows.com>
2013-11-15 01:20:16 -0700

    Added comments and cleaned up a bit, final commit for Thursday night.

--- CSE430_Project2/Project2.cpp
+++ CSE430_Project2/Project2.cpp
@@ -1,23 +1,36 @@
 #include <cstdio>
 #include <cstdlib>
-#include <iostream>
-#include <string>
 #include <fstream>
 
 using namespace std;
 
 bool readGraph(const char* graphFile)
 {
+	// Open the graphFile 
 	ifstream myFile(graphFile);
+
 	int vertices;
 	myFile >> vertices;
-	myFile >> vertices;
+
+	// Close the graph.txt file
 	myFile.close();
-	return false;
+	// File successfully read
+	return true;
+}
+
+bool readRequests(const char* requestsFile)
+{
+	// Open the requestsFile
+	ifstream myFile(requestsFile);
+
+	// Close the requests.txt file
+	myFile.close();
+	// File successfully read
+	return true;
 }
 
 int main( int argc, const char* argv[] )
 {
 	readGraph(argv[1]);
-	printf( "\nHello World\n" );
+	readRequests(argv[2]);
 }
